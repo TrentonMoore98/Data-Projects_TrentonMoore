8c5def0d8386341b3f9db7fd147ce7f904517c49
TrentonMoore98 <70575986+TrentonMoore98@users.noreply.github.com>
2021-07-12 17:42:22 -0400

    Update Dimension And Fact Tables.sql

--- Sales Report Practice Project/Dimension And Fact Tables.sql
+++ Sales Report Practice Project/Dimension And Fact Tables.sql
@@ -15,21 +15,22 @@ SELECT
   Quarter, 
   Year 
 FROM 
-  `sales-report-dashboard-project.DIM.DIM_DATE`
-WHERE Year < 2021;
+  `sales-report-dashboard-project.DIM.DIM_DATE` 
+WHERE 
+  Year < 2021;
 
 
 -- Cleansed DIM_Customers Table --
 -- DIM_Customer --
 SELECT 
-  Customer.CustomerKey, 
+  customer.CustomerKey, 
   First_Name, 
   Last_Name, 
   CONCAT(First_Name, " ", Last_Name) AS Full_Name, 
   Gender, 
   DateFirstPurchase, 
   Customer_City, 
-  Sales.SalesAmount 
+  Sales.SalesAmount -- Joined in Sales from the Internet Sales table
 FROM 
   `sales-report-dashboard-project.DIM.DIM_CUSTOMERS` AS Customer 
   LEFT JOIN `sales-report-dashboard-project.FACT.FACT_InternetSales` AS Sales ON customer.customerkey = sales.customerkey 
@@ -37,7 +38,43 @@ WHERE
   SalesAmount IS NOT NULL 
   AND DateFirstPurchase < '2021-01-01' 
 ORDER BY 
-  salesamount;
+  CustomerKey ASC; -- Ordered list by CustomerKey
+
+
+-- Cleansed DIM_ProductsSold Table --
+-- DIM_Product --
+
+SELECT 
+  p.ProductKey, 
+  p.ProductItemCode, 
+  p.Product_Name, 
+  p.Sub_Category, 
+  p.Product_Category, 
+  p.Product_Color, 
+  p.Product_Size, 
+  p.Product_Line, 
+  p.Product_Model_Name, 
+  p.Product_Description, 
+  p.Product_Status, 
+FROM 
+  `sales-report-dashboard-project.DIM.DIM_PRODUCTS` AS p 
+ORDER BY 
+  p.ProductKey ASC;
+
+
+-- Cleansed FACT_InternetSales --
+-- FACT_Sales --
+
+SELECT 
+  ProductKey AS Product_Key,
+  OrderDateKey,
+  DueDateKey,
+  ShipDateKey,
+  CustomerKey,
+  SalesOrderNumber,
+  SalesAmount
+FROM `sales-report-dashboard-project.FACT.FACT_InternetSales`;
+
 
 
 
