638f86cc9348db4a5df58e787c90a16619d47cd6
TrentonMoore98 <70575986+TrentonMoore98@users.noreply.github.com>
2022-03-04 00:12:52 -0500

    Update Email Analysis.sql

--- Email Marketing Challenge/Email Analysis.sql
+++ Email Marketing Challenge/Email Analysis.sql
@@ -1,1 +1,21 @@
 
+## What time of day has the highest open rate?
+## Which Campaign has the lowest click through/open rate?
+## Specific days of the week that has the highest click through rate?
+## Which campaign list is engaging the most with our emails?
+
+# Two main KPIs: Open rate AND Click through rate
+
+# Product discount play any role?
+
+# Important dimensions:
+## Campaign (individual email sent)
+## Campaign list (type of email recipient)
+## TIME !!! 
+
+WITH
+
+
+SELECT *
+FROM `emailmarketing-343102.EmailDataset.Email` 
+LIMIT 10
